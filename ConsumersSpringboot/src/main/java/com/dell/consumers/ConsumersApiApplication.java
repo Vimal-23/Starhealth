40ee4d7ef9a38dba9e5e531961fd28b231e74e70
Vimalkanna M <Vimalkanna.m@starhealth.in>
2022-10-18 19:38:16 +0530

    Spring project

---
+++ ConsumersSpringboot/src/main/java/com/dell/consumers/ConsumersApiApplication.java
@@ -0,0 +1,12 @@
+package com.dell.consumers;
+
+
+
+@SpringBootApplication
+public class ConsumersApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumersApiApplication.class, args);
+	}
+
+}
---
+++ ConsumersSpringboot/src/test/java/com/dell/consumers/ConsumersApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dell.consumers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumersApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
